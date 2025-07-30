eee19054ff64c19582e82718fc4a85ce10cbf0d5
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-07-30 08:55:29 -0600

    Initial Commit

---
+++ AnimalCrossing/AnimalCrossingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalCrossingApp.swift
+//  AnimalCrossing
+//
+//  Created by Benjamin Rojo on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimalCrossingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
