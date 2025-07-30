b2489336824cf7edbc5df0e708498135a7586fd3
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-07-30 09:28:23 -0600

    i practice @State

--- AnimalCrossing/ContentView.swift
+++ AnimalCrossing/ContentView.swift
@@ -14,11 +14,50 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+        Tarjeta()
         }
         .padding()
     }
 }
 
+struct Datos: Identifiable{
+    let id = UUID()
+    let fruta: String
+    let image: String
+    var meGusta: Bool
+}
+
+struct Tarjeta: View{
+    @State var frutas : [Datos] = [
+        Datos(fruta: "Melon", image: "melon", meGusta: true),
+        Datos(fruta: "Sandia", image: "melon", meGusta: true),
+        Datos(fruta: "Manzana", image: "melon", meGusta: true),
+        Datos(fruta: "Papaya", image: "melon", meGusta: true)
+    ]
+    var body: some View{
+        ScrollView{
+            VStack{
+                ForEach(frutas.indices, id:\.self){ index in
+                    VStack{
+                        Image(frutas[index].image)
+                            .resizable()
+                            .frame(height: 150)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                        Text(frutas[index].fruta)
+                            .font(.title2)
+                        Button(action:{
+                            frutas[index].meGusta.toggle()
+                        }){
+                            Image(systemName: frutas[index].meGusta ? "heart.fill" : "heart")
+                            .foregroundColor(.red)
+                            .font(.title)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 #Preview {
     ContentView()
 }
